3576ada490ae49f02cebcb768ed78d123c2d778a
cristopher7 <cristopher.torres@usach.cl>
2026-01-03 04:56:25 -0300

    cambios

--- ToolRent_FRONTEND/src/services/keycloak.js
+++ ToolRent_FRONTEND/src/services/keycloak.js
@@ -3,7 +3,7 @@ import Keycloak from "keycloak-js";
 
 
 const keycloak = new Keycloak({
-  url: "http://172.23.14.168:31147",  
+  url: "http://172.23.14.168:30361",  
   realm: "tingeso-realm",              // Confirmado en tu URL de Keycloak
   clientId: "toolrent-frontend"        // Asegúrate de que este sea el ID que creaste
 });
